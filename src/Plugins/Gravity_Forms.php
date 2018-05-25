8974a2a185afe1b2637e0132d5d37e0ae0fc4644
Travis Smith <travis.smith@weather.com>
2018-05-25 15:30:19 -0400

    Docs & WPCS

--- src/Plugins/Gravity_Forms.php
+++ src/Plugins/Gravity_Forms.php
@@ -1,13 +1,45 @@
 <?php
+/**
+ * Gravity Forms Class
+ *
+ * Extends Gravity Forms.
+ *
+ * You may copy, distribute and modify the software as long as you track changes/dates in source files.
+ * Any modifications to or software including (via compiler) GPL-licensed code must also be made
+ * available under the GPL along with build & install instructions.
+ *
+ * @package    WPS\Core
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2015-2018 Travis Smith
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ * @link       https://github.com/wpsmith/WPS
+ * @version    1.0.0
+ * @since      0.1.0
+ */
 
 namespace WPS\Plugins;
 
 use WPS\Core\Singleton;
 
-class Gravity_Forms extends Singleton {
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	public function __construct() {
-		add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+if ( ! class_exists( 'WPS\Plugins\Gravity_Forms' ) ) {
+	/**
+	 * Class Gravity_Forms
+	 *
+	 * @package WPS\Plugins
+	 */
+	class Gravity_Forms extends Singleton {
+
+		/**
+		 * GravityForms constructor.
+		 */
+		public function __construct() {
+			add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+		}
+
 	}
-
 }
