984bb4078f1c70cb5781c55515a9938dea9798b6
Travis Smith <travis.smith@weather.com>
2018-05-25 15:34:08 -0400

    Van. Docs & WPCS. Use new_fields_builder method.

--- src/PostTypes/Van.php
+++ src/PostTypes/Van.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace WPS\PostTypes;
+namespace Site\PostTypes;
 
-use WPS\Core;
-use StoutLogic\AcfBuilder\FieldsBuilder;
+use WPS;
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -11,184 +10,310 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-class Van extends Core\Post_Type {
+if ( ! class_exists( 'Van' ) ) {
+	class Van extends WPS\Core\Post_Type {
 
-	public $post_type = 'van';
+		/**
+		 * Post Type registered name
+		 *
+		 * @var string
+		 */
+		public $post_type = 'van';
 
-	public function __construct() {
-		add_action( 'init', array( $this, 'create_post_type' ), 0 );
-		add_action( 'init', array( $this, 'create_taxonomy' ), 0 );
+		/**
+		 * Plural Post Type registered name
+		 *
+		 * @var string
+		 */
+		public $plural = 'vans';
 
-		add_action( 'genesis_header', array( $this, 'remove_post_type_meta' ) );
-//		add_filter( "manage_{$this->post_type}_posts_columns", array( $this, 'manage_posts_columns' ) );
-//		add_action( "manage_{$this->post_type}_posts_custom_column", array( $this, 'manage_posts_custom_column' ), 10, 2 );
+		/**
+		 * Remove Genesis Post Type Meta.
+		 * @var bool
+		 */
+		public $remove_post_type_entry_meta = true;
 
-		add_action( 'core_acf_fields', array( $this, 'core_acf_fields' ) );
-	}
+		/**
+		 * Envira Gallery Support.
+		 *
+		 * @var bool
+		 */
+		public $gallery = true;
 
-	public function core_acf_fields( $fields ) {
-		$content = new FieldsBuilder( $this->post_type );
-		$content
-			->addText( 'make' )
-			->addText( 'model' )
-			->addText( 'slide_number_value', array(
-				'label'       => __( 'Slide Number Value', SITE_MU_TEXT_DOMAIN ),
-				'description' => __( 'Define slide order. Ex. 1,2,3,4,...', SITE_MU_TEXT_DOMAIN ),
-			) )
-			->setLocation( 'post_type', '==', $this->post_type );
-
-		$fields->builder[] = $content;
-	}
-
-	/**
-	 * Register custom post type
-	 */
-	public function create_post_type() {
-
-		$labels   = array(
-			'name'                  => _x( 'Vans', 'Post Type General Name', SITE_MU_TEXT_DOMAIN ),
-			'singular_name'         => _x( 'Van', 'Post Type Singular Name', SITE_MU_TEXT_DOMAIN ),
-			'menu_name'             => __( 'Vans', SITE_MU_TEXT_DOMAIN ),
-			'name_admin_bar'        => __( 'Vans', SITE_MU_TEXT_DOMAIN ),
-			'archives'              => __( 'Van Archives', SITE_MU_TEXT_DOMAIN ),
-			'attributes'            => __( 'Van Attributes', SITE_MU_TEXT_DOMAIN ),
-			'parent_item_colon'     => __( 'Parent Van:', SITE_MU_TEXT_DOMAIN ),
-			'all_items'             => __( 'All Van', SITE_MU_TEXT_DOMAIN ),
-			'add_new_item'          => __( 'Add New Van', SITE_MU_TEXT_DOMAIN ),
-			'add_new'               => __( 'Add New', SITE_MU_TEXT_DOMAIN ),
-			'new_item'              => __( 'New Van', SITE_MU_TEXT_DOMAIN ),
-			'edit_item'             => __( 'Edit Van', SITE_MU_TEXT_DOMAIN ),
-			'update_item'           => __( 'Update Van', SITE_MU_TEXT_DOMAIN ),
-			'view_item'             => __( 'View Van', SITE_MU_TEXT_DOMAIN ),
-			'view_items'            => __( 'View Van', SITE_MU_TEXT_DOMAIN ),
-			'search_items'          => __( 'Search Van', SITE_MU_TEXT_DOMAIN ),
-			'not_found'             => __( 'Not found', SITE_MU_TEXT_DOMAIN ),
-			'not_found_in_trash'    => __( 'Not found in Trash', SITE_MU_TEXT_DOMAIN ),
-			'featured_image'        => __( 'Featured Image', SITE_MU_TEXT_DOMAIN ),
-			'set_featured_image'    => __( 'Set featured image', SITE_MU_TEXT_DOMAIN ),
-			'remove_featured_image' => __( 'Remove featured image', SITE_MU_TEXT_DOMAIN ),
-			'use_featured_image'    => __( 'Use as featured image', SITE_MU_TEXT_DOMAIN ),
-			'insert_into_item'      => __( 'Insert into van', SITE_MU_TEXT_DOMAIN ),
-			'uploaded_to_this_item' => __( 'Uploaded to this van', SITE_MU_TEXT_DOMAIN ),
-			'items_list'            => __( 'Van list', SITE_MU_TEXT_DOMAIN ),
-			'items_list_navigation' => __( 'Van list navigation', SITE_MU_TEXT_DOMAIN ),
-			'filter_items_list'     => __( 'Filter van list', SITE_MU_TEXT_DOMAIN ),
+		/**
+		 * Args for the gallery shortcode.
+		 *
+		 * @var array
+		 */
+		public $gallery_args = array(
+			'size'    => 'van',
+			'columns' => 4,
+			'link'    => 'file',
 		);
-		$rewrite  = array(
-			'slug'       => $this->post_type,
-			'with_front' => true,
-			'pages'      => true,
-			'feeds'      => true,
-		);
-		$supports = array(
-			'title',
-			'editor',
-			'excerpt',
-			'thumbnail',
-			'genesis-seo',
-			'genesis-scripts',
-			'genesis-layouts',
-			'genesis-cpt-archives-settings',
-			'genesis-simple-sidebars',
-		);
-		$args     = array(
-			'label'               => __( 'Vans', SITE_MU_TEXT_DOMAIN ),
-			'description'         => __( 'For Vans', SITE_MU_TEXT_DOMAIN ),
-			'labels'              => $labels,
-			'supports'            => $supports,
-			'hierarchical'        => true,
-			'public'              => true,
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'menu_position'       => 6.9,
-			'menu_icon'           => 'dashicons-star-filled',
-			'show_in_admin_bar'   => true,
-			'show_in_nav_menus'   => true,
-			'can_export'          => true,
-			'has_archive'         => true,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'rewrite'             => $rewrite,
-			'capability_type'     => 'page',
-			'show_in_rest'        => true,
-		);
-		register_post_type( $this->post_type, $args );
 
-//	new WPS\Schema\Entry_Schema( $this->post_type, 'video' );
+		/**
+		 * Template Loader.
+		 *
+		 * @var WPS\Templates\Template_Loader
+		 */
+		public $template_loader;
 
-//	new WPS\Templates\Simple_Sidebars( $this->post_type );
-	}
+		/**
+		 * Whether to create a related types taxonomy.
+		 *
+		 * @var bool
+		 */
+		public $types = true;
 
-	/**
-	 * Register Custom Taxonomy
-	 */
-	function create_taxonomy() {
-
-		$labels  = array(
-			'name'                       => _x( 'Types', 'Taxonomy General Name', SITE_MU_TEXT_DOMAIN ),
-			'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', SITE_MU_TEXT_DOMAIN ),
-			'menu_name'                  => __( 'Types', SITE_MU_TEXT_DOMAIN ),
-			'all_items'                  => __( 'All Items', SITE_MU_TEXT_DOMAIN ),
-			'parent_item'                => __( 'Parent Item', SITE_MU_TEXT_DOMAIN ),
-			'parent_item_colon'          => __( 'Parent Item:', SITE_MU_TEXT_DOMAIN ),
-			'new_item_name'              => __( 'New Item Name', SITE_MU_TEXT_DOMAIN ),
-			'add_new_item'               => __( 'Add New Item', SITE_MU_TEXT_DOMAIN ),
-			'edit_item'                  => __( 'Edit Item', SITE_MU_TEXT_DOMAIN ),
-			'update_item'                => __( 'Update Item', SITE_MU_TEXT_DOMAIN ),
-			'view_item'                  => __( 'View Item', SITE_MU_TEXT_DOMAIN ),
-			'separate_items_with_commas' => __( 'Separate items with commas', SITE_MU_TEXT_DOMAIN ),
-			'add_or_remove_items'        => __( 'Add or remove items', SITE_MU_TEXT_DOMAIN ),
-			'choose_from_most_used'      => __( 'Choose from the most used', SITE_MU_TEXT_DOMAIN ),
-			'popular_items'              => __( 'Popular Items', SITE_MU_TEXT_DOMAIN ),
-			'search_items'               => __( 'Search Items', SITE_MU_TEXT_DOMAIN ),
-			'not_found'                  => __( 'Not Found', SITE_MU_TEXT_DOMAIN ),
-			'no_terms'                   => __( 'No items', SITE_MU_TEXT_DOMAIN ),
-			'items_list'                 => __( 'Items list', SITE_MU_TEXT_DOMAIN ),
-			'items_list_navigation'      => __( 'Items list navigation', SITE_MU_TEXT_DOMAIN ),
-		);
-		$rewrite = array(
-			'slug'         => 'van-type',
-			'with_front'   => true,
-			'hierarchical' => true,
-		);
-		$args    = array(
-			'labels'            => $labels,
-			'hierarchical'      => true,
-			'public'            => false,
-			'show_ui'           => true,
-			'show_admin_column' => true,
-			'show_in_nav_menus' => false,
-			'show_tagcloud'     => false,
-			'rewrite'           => $rewrite,
-			'show_in_rest'      => true,
-		);
-		register_taxonomy( 'van-type', array( $this->post_type ), $args );
-
-	}
-
-	public function remove_post_type_meta() {
-		if ( $this->post_type === get_post_type() ) {
-			WPS\remove_post_type_meta();
+		/**
+		 * Initialize dependent post types.
+		 */
+		public function plugins_loaded() {
+			Manual::get_instance();
+			Video::get_instance();
 		}
-	}
 
-	// manage columns
-	public function manage_posts_columns( $columns ) {
-		return array(
-			'cb'        => '<input type="checkbox" />',
-			'thumbnail' => __( 'Thumbnail', SITE_MU_TEXT_DOMAIN ),
-			'title'     => __( 'Title', SITE_MU_TEXT_DOMAIN ),
-			'date'      => __( 'Date', SITE_MU_TEXT_DOMAIN ),
-		);
-	}
+		/**
+		 * Van initializer.
+		 */
+		public function init() {
 
-	public function manage_posts_custom_column( $column, $post_id ) {
-		switch ( $column ) {
-			case 'thumbnail' :
-				if ( has_post_thumbnail( $post_id ) ) {
-					echo get_the_post_thumbnail( $post_id, array( 50, 50 ) );
+			\WPS\add_image_size( 'van', 600, 450, true );
+
+			// Fitvids
+			WPS\Scripts\Fitvids::get_instance();
+
+			add_action( 'genesis_entry_content', array( $this, 'gallery_title' ), 14 );
+			add_action( 'genesis_entry_content', array( $this, 'field_manual' ), 18 );
+			add_action( 'genesis_entry_content', array( $this, 'videos' ), 20 );
+
+		}
+
+		/**
+		 * Add ACF fields to the Page.
+		 *
+		 * @param $fields
+		 */
+		public function core_acf_fields( $fields ) {
+
+			$specs = $this->new_fields_builder( 'specifications' );
+			$specs
+				->addText( 'make', array(
+					'label' => __( 'Make', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'model', array(
+					'label' => __( 'Model', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'ramp-width', array(
+					'label' => __( 'Ramp width (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'ramp-length', array(
+					'label' => __( 'Ramp length (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'ramp-angle', array(
+					'label' => __( 'Ramp angle (degrees)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'door-opening-height', array(
+					'label' => __( 'Door opening height (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'interior-smallest-width', array(
+					'label' => __( 'Interior width smallest (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'interior-depth', array(
+					'label' => __( 'Interior depth (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addText( 'ground-clearance', array(
+					'label' => __( 'Ground Clearance (inches)', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->setLocation( 'post_type', '==', $this->post_type );
+
+			$column_choices = array(
+				'choices' => array(
+					array( '' => __( 'Full Width', SITE_MU_TEXT_DOMAIN ) ),
+					array( 'one-half' => __( 'One Half', SITE_MU_TEXT_DOMAIN ) ),
+					array( 'one-third' => __( 'One Third', SITE_MU_TEXT_DOMAIN ) ),
+					array( 'one-fourth' => __( 'One Fourth', SITE_MU_TEXT_DOMAIN ) ),
+					array( 'one-sixth' => __( 'One Sixth', SITE_MU_TEXT_DOMAIN ) ),
+				),
+			);
+
+			$manuals = $this->new_fields_builder( 'manuals' );
+			$manuals
+				->addText( 'manual_title', array(
+					'default'     => __( 'Field Manual', SITE_MU_TEXT_DOMAIN ),
+					'placeholder' => __( 'Field Manual', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addRelationship( 'manuals_vans', array(
+					'label'                   => __( 'Connected Manuals & Vans', SITE_MU_TEXT_DOMAIN ),
+					'acf_relationship_create' => 1,
+					'post_type'               => array( 'manual', )
+				) )
+				->setLocation( 'post_type', '==', $this->post_type );
+
+			$galleries = $this->new_fields_builder( 'galleries' );
+			$galleries
+				->addText( 'gallery_title', array(
+					'default'     => __( 'Gallery', SITE_MU_TEXT_DOMAIN ),
+					'placeholder' => __( 'Gallery', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addSelect( 'gallery_columns', $column_choices )
+				->setDefaultValue( 'one-third' )
+				->addGallery( 'gallery' )
+				->setLocation( 'post_type', '==', $this->post_type );
+
+			$videos = $this->new_fields_builder( 'videos' );
+			$videos
+				->addText( 'videos_title', array(
+					'default'     => __( 'Repair Videos', SITE_MU_TEXT_DOMAIN ),
+					'placeholder' => __( 'Repair Videos', SITE_MU_TEXT_DOMAIN ),
+				) )
+				->addSelect( 'video_columns', $column_choices )
+				->setDefaultValue( 'one-third' )
+				->addRelationship( 'videos_vans', array(
+					'label'                   => __( 'Connected Videos & Vans', SITE_MU_TEXT_DOMAIN ),
+					'acf_relationship_create' => 1,
+					'post_type'               => array( 'video', )
+				) )
+				->setLocation( 'post_type', '==', $this->post_type );
+
+			$fields->builder[] = $specs;
+			$fields->builder[] = $manuals;
+			$fields->builder[] = $galleries;
+			$fields->builder[] = $videos;
+		}
+
+		/**
+		 * Register custom post type
+		 */
+		public function create_post_type() {
+
+			$this->register_post_type( array(
+				'hierarchical'        => true,
+				'public'              => true,
+				'show_ui'             => true,
+				'show_in_menu'        => true,
+				'menu_position'       => 6.9,
+				'menu_icon'           => 'dashicons-star-filled',
+				'show_in_admin_bar'   => true,
+				'show_in_nav_menus'   => true,
+				'can_export'          => true,
+				'has_archive'         => true,
+				'exclude_from_search' => false,
+				'publicly_queryable'  => true,
+				'capability_type'     => 'page',
+				'show_in_rest'        => true,
+			) );
+
+			new WPS\Schema\Entry_Schema( $this->post_type, 'vehicle' );
+
+		}
+
+		/**
+		 * Gets supports array.
+		 *
+		 * @return array Array of post type supports.
+		 */
+		protected function get_supports() {
+			return array(
+				'title',
+				'editor',
+				'excerpt',
+				'thumbnail',
+//			'custom-fields',
+				'genesis-seo',
+//			'genesis-scripts',
+//			'genesis-layouts',
+				'genesis-cpt-archives-settings',
+//			'genesis-simple-sidebars',
+			);
+		}
+
+		public function field_manual() {
+			if ( ! $this->is_post_type() || ! ( $manuals_vans = get_post_meta( get_the_ID(), 'manuals_vans', true ) ) ) {
+				return;
+			}
+
+			WPS\printr($manuals_vans);
+
+			// The Field Manual
+			if( rcp_is_active() ) {
+
+				// Manual Title
+				$title = get_post_meta( get_the_ID(), 'manual_title', true );
+				$title = $title ? $title : _n( 'Field Manual', 'Field Manuals', count( $manuals_vans ), SITE_MU_TEXT_DOMAIN );
+
+				// Field Slug
+				$slug  = _n( 'manual', 'manuals', count( $manuals_vans ), SITE_MU_TEXT_DOMAIN );
+				$slug = count( $manuals_vans ) > 1 ? $slug . '/' : $slug . '/' . get_post( $manuals_vans[0] )->post_name . '/';
+
+				// The HTML
+				echo '<div class="field-manual full-width clearfix" style="background-color: #141e28;color: #fff; padding: 2em 0 1em; margin-bottom: 1.5em;"><div class="content">';
+				printf( '<div class="first two-thirds"><h3 style="color: #fff; margin: 0.5em 0;">%s</h3></div>', $title );
+				echo '<div class="one-third"><div class="wrap">';
+				printf( '<a target="_blank" href="%s" data-id="%s" class="button accent alignright">%s</a>', get_permalink() . $slug, $manuals_vans[0], __( 'Open', SITE_MU_TEXT_DOMAIN ) );
+				echo '</div></div>';
+				echo '</div></div>';
+
+			}
+
+		}
+
+
+		public function videos() {
+			if ( ! $this->is_post_type() || ! ( $videos_vans = get_post_meta( get_the_ID(), 'videos_vans', true ) ) ) {
+				return;
+			}
+
+			if( rcp_is_active() ) {
+
+				// Do Title
+				$title = get_post_meta( get_the_ID(), 'videos_title', true );
+				$title = $title ? $title : __( 'Repair Videos', SITE_MU_TEXT_DOMAIN );
+				printf( '<h3>%s</h3>', $title );
+
+				// Do Videos
+				$columns = $this->get_columns( 'video_columns' );
+				$counter = 0;
+				echo '<div class="videos">';
+				foreach ( $videos_vans as $video_id ) {
+					$video        = get_post( $video_id );
+					$column_class = WPS\get_column_classes_by_column_class( $columns, $counter );
+					printf( '<div class="%s">%s</div>', $column_class, apply_filters( 'the_content', $video->post_content ) );
+					$counter ++;
+
 				}
-				break;
+
+				// Do view all button
+//			$slug = _n( 'video', 'videos', count( $videos_vans ), SITE_MU_TEXT_DOMAIN );
+//			echo '<div class="field-manual full-width clearfix" style="padding: 2em 0 1em; margin-bottom: 1.5em;"><div class="content">';
+//			echo '<div class="first two-thirds"><h3 style="margin: 0.5em 0;">&nbsp;</h3></div>';
+//			echo '<div class="one-third"><div class="wrap">';
+//			printf( '<a target="_blank" href="%s" class="button accent alignright">%s</a>', get_permalink() . $slug . '/', __( 'See All', SITE_MU_TEXT_DOMAIN ) );
+//			echo '</div></div>';
+//			echo '</div></div>';
+
+				echo '</div>';
+			}
 		}
+
+		public function gallery_title() {
+			if ( ! $this->is_post_type() ) {
+				return;
+			}
+
+			$title = get_post_meta( get_the_ID(), 'gallery_title', true );
+			$title = $title ? $title : __( 'Gallery', SITE_MU_TEXT_DOMAIN );
+			printf( '<h3>%s</h3>', $title );
+
+			$this->gallery_args['columns'] = WPS\get_column_class_num_by_column_class_name( $this->get_columns( 'gallery_columns' ) );
+		}
+
+		private function get_columns( $key ) {
+			$columns = get_post_meta( get_the_ID(), $key, true );
+			$columns = $columns ? $columns : 'one-third';
+
+			return $columns;
+		}
+
 	}
 }
