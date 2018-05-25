dc65b11ff7deb61f747df449b493ad2abdcba65d
Travis Smith <travis.smith@weather.com>
2018-05-25 15:33:10 -0400

    Product. Docs & WPCS. Use new_fields_builder method.

--- src/PostTypes/Product.php
+++ src/PostTypes/Product.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace WPS\PostTypes;
+namespace Site\PostTypes;
 
 use WPS\Core;
-use StoutLogic\AcfBuilder\FieldsBuilder;
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,7 +12,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Product extends Core\Post_Type {
 
+	/**
+	 * Post Type registered name
+	 *
+	 * @var string
+	 */
 	public $post_type = 'product';
+
+	/**
+	 * Whether to create a related types taxonomy.
+	 *
+	 * @var bool
+	 */
+	public $types = true;
 
 	public function init() {
 		add_filter( 'display_posts_shortcode_output', array( $this, 'display_posts_shortcode_output' ), 10, 9 );
@@ -30,7 +41,6 @@ class Product extends Core\Post_Type {
 	 * @param $content string, post content
 	 * @param $class array, post classes
 	 * @return $output string, the modified markup for an individual post
-
 	 */
 	public function display_posts_shortcode_output( $output, $atts, $image, $title, $date, $excerpt, $inner_wrapper, $content, $class ) {
 		if ( $this->post_type === get_post_type() ) {
@@ -53,7 +63,7 @@ class Product extends Core\Post_Type {
 //	}
 
 	public function core_acf_fields( $fields ) {
-		$content = new FieldsBuilder( $this->post_type );
+		$content = $this->new_fields_builder();
 		$content
 			->addText('cost')
 			->setLocation( 'post_type', '==', $this->post_type );
@@ -61,58 +71,22 @@ class Product extends Core\Post_Type {
 		$fields->builder[] = $content;
 	}
 
-	/**
-	 * Register custom post type
-	 */
-	public function create_post_type() {
-
-		$labels   = array(
-			'name'                  => _x( 'Products', 'Post Type General Name', SITE_MU_TEXT_DOMAIN ),
-			'singular_name'         => _x( 'Product', 'Post Type Singular Name', SITE_MU_TEXT_DOMAIN ),
-			'menu_name'             => __( 'Products', SITE_MU_TEXT_DOMAIN ),
-			'name_admin_bar'        => __( 'Products', SITE_MU_TEXT_DOMAIN ),
-			'archives'              => __( 'Item Archives', SITE_MU_TEXT_DOMAIN ),
-			'attributes'            => __( 'Item Attributes', SITE_MU_TEXT_DOMAIN ),
-			'parent_item_colon'     => __( 'Parent Item:', SITE_MU_TEXT_DOMAIN ),
-			'all_items'             => __( 'All Items', SITE_MU_TEXT_DOMAIN ),
-			'add_new_item'          => __( 'Add New Item', SITE_MU_TEXT_DOMAIN ),
-			'add_new'               => __( 'Add New', SITE_MU_TEXT_DOMAIN ),
-			'new_item'              => __( 'New Item', SITE_MU_TEXT_DOMAIN ),
-			'edit_item'             => __( 'Edit Item', SITE_MU_TEXT_DOMAIN ),
-			'update_item'           => __( 'Update Item', SITE_MU_TEXT_DOMAIN ),
-			'view_item'             => __( 'View Item', SITE_MU_TEXT_DOMAIN ),
-			'view_items'            => __( 'View Items', SITE_MU_TEXT_DOMAIN ),
-			'search_items'          => __( 'Search Item', SITE_MU_TEXT_DOMAIN ),
-			'not_found'             => __( 'Not found', SITE_MU_TEXT_DOMAIN ),
-			'not_found_in_trash'    => __( 'Not found in Trash', SITE_MU_TEXT_DOMAIN ),
-			'featured_image'        => __( 'Featured Image', SITE_MU_TEXT_DOMAIN ),
-			'set_featured_image'    => __( 'Set featured image', SITE_MU_TEXT_DOMAIN ),
-			'remove_featured_image' => __( 'Remove featured image', SITE_MU_TEXT_DOMAIN ),
-			'use_featured_image'    => __( 'Use as featured image', SITE_MU_TEXT_DOMAIN ),
-			'insert_into_item'      => __( 'Insert into item', SITE_MU_TEXT_DOMAIN ),
-			'uploaded_to_this_item' => __( 'Uploaded to this item', SITE_MU_TEXT_DOMAIN ),
-			'items_list'            => __( 'Items list', SITE_MU_TEXT_DOMAIN ),
-			'items_list_navigation' => __( 'Items list navigation', SITE_MU_TEXT_DOMAIN ),
-			'filter_items_list'     => __( 'Filter items list', SITE_MU_TEXT_DOMAIN ),
-		);
-		$rewrite  = array(
-			'slug'       => $this->post_type,
-			'with_front' => true,
-			'pages'      => true,
-			'feeds'      => true,
-		);
-		$supports = array(
+	protected function get_supports() {
+		return array(
 			'title',
 			'excerpt',
 			'thumbnail',
 			'genesis-seo',
 			'genesis-cpt-archives-settings',
 		);
-		$args     = array(
-			'label'               => __( 'Products', SITE_MU_TEXT_DOMAIN ),
-			'description'         => __( 'For Products', SITE_MU_TEXT_DOMAIN ),
-			'labels'              => $labels,
-			'supports'            => $supports,
+	}
+
+	/**
+	 * Register custom post type
+	 */
+	public function create_post_type() {
+
+		$this->register_post_type( array(
 			'hierarchical'        => false,
 			'public'              => true,
 			'show_ui'             => true,
@@ -128,59 +102,11 @@ class Product extends Core\Post_Type {
 			'rewrite'             => false,
 			'capability_type'     => 'post',
 			'show_in_rest'        => true,
-		);
-		register_post_type( $this->post_type, $args );
+		) );
 
 //	new WPS\Schema\Entry_Schema( $this->post_type, 'video' );
 
 //	new WPS\Templates\Simple_Sidebars( $this->post_type );
-	}
-
-	/**
-	 * Register Custom Taxonomy
-	 */
-	function create_taxonomy() {
-
-		$labels  = array(
-			'name'                       => _x( 'Types', 'Taxonomy General Name', SITE_MU_TEXT_DOMAIN ),
-			'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', SITE_MU_TEXT_DOMAIN ),
-			'menu_name'                  => __( 'Types', SITE_MU_TEXT_DOMAIN ),
-			'all_items'                  => __( 'All Items', SITE_MU_TEXT_DOMAIN ),
-			'parent_item'                => __( 'Parent Item', SITE_MU_TEXT_DOMAIN ),
-			'parent_item_colon'          => __( 'Parent Item:', SITE_MU_TEXT_DOMAIN ),
-			'new_item_name'              => __( 'New Item Name', SITE_MU_TEXT_DOMAIN ),
-			'add_new_item'               => __( 'Add New Item', SITE_MU_TEXT_DOMAIN ),
-			'edit_item'                  => __( 'Edit Item', SITE_MU_TEXT_DOMAIN ),
-			'update_item'                => __( 'Update Item', SITE_MU_TEXT_DOMAIN ),
-			'view_item'                  => __( 'View Item', SITE_MU_TEXT_DOMAIN ),
-			'separate_items_with_commas' => __( 'Separate items with commas', SITE_MU_TEXT_DOMAIN ),
-			'add_or_remove_items'        => __( 'Add or remove items', SITE_MU_TEXT_DOMAIN ),
-			'choose_from_most_used'      => __( 'Choose from the most used', SITE_MU_TEXT_DOMAIN ),
-			'popular_items'              => __( 'Popular Items', SITE_MU_TEXT_DOMAIN ),
-			'search_items'               => __( 'Search Items', SITE_MU_TEXT_DOMAIN ),
-			'not_found'                  => __( 'Not Found', SITE_MU_TEXT_DOMAIN ),
-			'no_terms'                   => __( 'No items', SITE_MU_TEXT_DOMAIN ),
-			'items_list'                 => __( 'Items list', SITE_MU_TEXT_DOMAIN ),
-			'items_list_navigation'      => __( 'Items list navigation', SITE_MU_TEXT_DOMAIN ),
-		);
-		$rewrite = array(
-			'slug'         => 'product-type',
-			'with_front'   => true,
-			'hierarchical' => true,
-		);
-		$args    = array(
-			'labels'            => $labels,
-			'hierarchical'      => true,
-			'public'            => false,
-			'show_ui'           => true,
-			'show_admin_column' => true,
-			'show_in_nav_menus' => false,
-			'show_tagcloud'     => false,
-			'rewrite'           => $rewrite,
-			'show_in_rest'      => true,
-		);
-		register_taxonomy( 'product-type', array( $this->post_type ), $args );
-
 	}
 
 	// manage columns
