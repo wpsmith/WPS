e870a60472b3d06b5e3eab91c5545ce97d31710e
Travis Smith <travis.smith@weather.com>
2018-05-25 15:42:14 -0400

    Custom HTML Widget

--- src/Widgets/Custom_HTML.php
+++ src/Widgets/Custom_HTML.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Widget Custom HTML Class File
+ *
+ * You may copy, distribute and modify the software as long as you track changes/dates in source files.
+ * Any modifications to or software including (via compiler) GPL-licensed code must also be made
+ * available under the GPL along with build & install instructions.
+ *
+ * @package    WPS\Widgets
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2015-2018 Travis Smith
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ * @link       https://github.com/wpsmith/WPS
+ * @version    1.0.0
+ * @since      0.1.0
+ */
 
 namespace WPS\Widgets;
 
