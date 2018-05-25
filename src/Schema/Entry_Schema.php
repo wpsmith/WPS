e657256b130d0deca9e1501734c98e4c9abb8c0f
Travis Smith <travis.smith@weather.com>
2018-05-25 15:30:58 -0400

    Docs & WPCS

--- src/Schema/Entry_Schema.php
+++ src/Schema/Entry_Schema.php
@@ -1,46 +1,52 @@
 <?php
 /**
- * WP Smith Schema Class
+ * Schema File
  *
- * @package   WPS_Core
- * @author    Travis Smith <t@wpsmith.net>
- * @license   GPL-2.0+
- * @link      http://wpsmith.net
- * @copyright 2014 Travis Smith, WP Smith, LLC
+ * Assist in setting up cron jobs within WordPress.
+ *
+ * You may copy, distribute and modify the software as long as you track changes/dates in source files.
+ * Any modifications to or software including (via compiler) GPL-licensed code must also be made
+ * available under the GPL along with build & install instructions.
+ *
+ * @package    WPS\Schema
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2015-2018 Travis Smith
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ * @link       https://github.com/wpsmith/WPS
+ * @version    1.0.0
+ * @since      0.1.0
  */
 
 namespace WPS\Schema;
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'Entry_Schema' ) ) {
+if ( ! class_exists( 'WPS\Schema\Entry_Schema' ) ) {
 	/**
-	 * Core Plugin class.
+	 * Class Entry_Schema
 	 *
-	 * The class handles the version, slug, and instance of all the
-	 * classes that extend it.
-	 *
-	 * @package WPS_Core
-	 * @author  Travis Smith <t@wpsmith.net>
+	 * @package WPS\Schema
 	 */
 	class Entry_Schema extends Schema_Core {
 
+		/**
+		 * Initializes class.
+		 */
 		public function init() {
 
-			// This assumes the the context is the post type
+			// This assumes the the context is the post type.
 			if ( isset( $this->attributes['empty'] ) ) {
 				unset( $this->attributes['empty'] );
 				add_filter( 'genesis_attr_entry', array( $this, 'remove' ), 11 );
 			}
 
-			
 			add_filter( 'genesis_attr_entry', array( $this, 'schema' ), 20, 2 );
 			add_filter( 'genesis_attr_entry-author-name', array( $this, 'author_name' ), 20, 2 );
 
-			switch( $this->schema ) {
+			switch ( $this->schema ) {
 				case 'location':
 					add_filter( 'genesis_attr_entry-address', array( $this, 'schema' ), 20, 2 );
 					add_filter( 'genesis_attr_entry-street', array( $this, 'schema' ), 20, 2 );
@@ -50,7 +56,7 @@ if ( ! class_exists( 'Entry_Schema' ) ) {
 					break;
 			}
 
-			// Fixes, @link
+			// Fixes, @link.
 			if ( post_type_supports( $this->post_type, 'title' ) && post_type_supports( $this->post_type, 'author' ) ) {
 				add_filter( 'genesis_post_title_output', array( $this, 'title_link' ), 20 );
 			}
--- src/Schema/Schema_Core.php
+++ src/Schema/Schema_Core.php
@@ -1,174 +1,245 @@
 <?php
 /**
- * WP Smith Schema Class
+ * Schema Core Abstract Class
  *
- * @package   WPS_Core
- * @author    Travis Smith <t@wpsmith.net>
- * @license   GPL-2.0+
- * @link      http://wpsmith.net
- * @copyright 2014 Travis Smith, WP Smith, LLC
+ * Base for dealing with schemas in Genesis.
+ *
+ * You may copy, distribute and modify the software as long as you track changes/dates in source files.
+ * Any modifications to or software including (via compiler) GPL-licensed code must also be made
+ * available under the GPL along with build & install instructions.
+ *
+ * @package    WPS\Schema
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2015-2018 Travis Smith
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ * @link       https://github.com/wpsmith/WPS
+ * @version    1.0.0
+ * @since      0.1.0
  */
 
 namespace WPS\Schema;
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Core Plugin class.
- *
- * The class handles the version, slug, and instance of all the
- * classes that extend it.
- *
- * @package WPS_Core
- * @author  Travis Smith <t@wpsmith.net>
- */
-abstract class Schema_Core {
-
-	public $post_type;
-	public $schema;
-	public $attributes = array();
-
+if ( ! class_exists( 'WPS\Schema\Schema_Core' ) ) {
 	/**
-	 * Constructor method
+	 * Class Schema_Core.
 	 *
-	 * @since  1.0.0
-	 * @date   2014-06-05
-	 * @author Travis Smith <t(at)wpsmith.net>}
-	 *
-	 * @param  string $type Schema context.
-	 * @param  array $attributes Array of attributes to add.
-	 *
-	 * @access private
+	 * @package WPS\Schema
 	 */
-	public function __construct( $type, $schema = '', $attributes = array() ) {
-		$this->schema = $schema = '' === $schema ? $type : $schema;
+	abstract class Schema_Core {
 
-		// Store Post Type
-		$this->post_type = $type;
+		/**
+		 * Post Type.
+		 *
+		 * @var string
+		 */
+		public $post_type;
 
-		// Save Schema
-		if ( ! empty( $this->get_schema( $schema ) ) ) {
-			$this->attributes = wp_parse_args( $attributes, $this->get_schema( $schema ) );
-		} else {
-			$this->attributes = $attributes;
+		/**
+		 * Schema.
+		 *
+		 * @var string
+		 */
+		public $schema;
+
+		/**
+		 * Schema Attributes.
+		 *
+		 * @var array
+		 */
+		public $attributes = array();
+
+		/**
+		 * Schema_Core constructor.
+		 *
+		 * @param string $post_type  Post Type.
+		 * @param string $schema     Schema name.
+		 * @param array  $attributes Schema attributes.
+		 */
+		public function __construct( $post_type, $schema = '', $attributes = array() ) {
+			$schema       = '' === $schema ? $post_type : $schema;
+			$this->schema = $schema;
+
+			// Store Post Type.
+			$this->post_type = $post_type;
+
+			// Save Schema.
+			$s = $this->get_schema( $schema );
+			if ( ! empty( $s ) ) {
+				$this->attributes = wp_parse_args( $attributes, $s );
+			} else {
+				$this->attributes = $attributes;
+			}
+
+			if ( method_exists( $this, 'init' ) ) {
+				add_action( 'wp_loaded', array( $this, 'init' ) );
+			}
+
 		}
 
-		if ( method_exists( $this, 'init' ) ) {
-			add_action( 'wp_loaded', array( $this, 'init' ) );
+		/**
+		 * Gets schemas.
+		 *
+		 * @param string $schema Schema.
+		 *
+		 * @return array Array of schemas.
+		 */
+		protected function get_schema( $schema ) {
+
+			$schemas = Schemas::get_instance();
+
+			return $schemas->get_schema( $schema );
+
 		}
 
-	}
+		/**
+		 * Gets an array of genesis HTML5 entry hooks.
+		 *
+		 * @return array
+		 */
+		protected function get_genesis_attr_entry_hooks() {
+			return array(
+				'entry',
+				'entry-title',
+				'entry-content',
+				'entry-author',
+			);
 
-	protected function get_schema( $schema ) {
-		return Schemas::get_instance()->get_schema( $schema );
-
-	}
-
-	protected function get_genesis_attr_entry_hooks() {
-		return array(
-//				'content',
-			'entry',
-			'entry-title',
-			'entry-content',
-			'entry-author',
-		);
-
-	}
-
-	public function add_schema( $context, $attributes ) {
-		if (
-			! empty( $this->post_type ) &&
-			( is_array( $this->attributes ) &&
-			  ! empty( $this->attributes ) )
-		) {
-			$attributes = wp_parse_args( $this->attributes, $attributes );
 		}
 
-		$class = ( false !== strpos( $attributes['class'], $this->post_type ) ) ? trim( $attributes['class'] . ' ' . $this->post_type ) : $attributes['class'];
+		/**
+		 * Adds schema to Genesis attributes.
+		 *
+		 * @param string $context    Context.
+		 * @param array  $attributes Array of HTML elements.
+		 *
+		 * @return array
+		 */
+		public function add_schema( $context, $attributes ) {
+			if (
+				! empty( $this->post_type ) &&
+				(
+					is_array( $this->attributes ) &&
+					! empty( $this->attributes )
+				)
+			) {
+				$attributes = wp_parse_args( $this->attributes, $attributes );
+			}
 
-		$attributes['class'] = apply_filters( 'wps_schema_class_' . $this->post_type, $class, $attributes );
+			$class = ( false !== strpos( $attributes['class'], $this->post_type ) ) ? trim( $attributes['class'] . ' ' . $this->post_type ) : $attributes['class'];
 
-		return $attributes;
-	}
+			$attributes['class'] = apply_filters( 'wps_schema_class_' . $this->post_type, $class, $attributes );
 
-	/**
-	 * Add attributes for site title element.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $attributes Existing attributes.
-	 *
-	 * @return array Amended attributes.
-	 */
-	public function schema( $attributes = array(), $context = '' ) {
-
-		// Make sure we have the correct post type
-		if ( $this->post_type !== get_post_type() ) {
 			return $attributes;
 		}
 
-		switch ( $context ) {
-			case 'entry':
-			case 'title':
-			case 'content':
-			case 'author':
-				return $this->add_schema( $context, $attributes );
-			default:
+		/**
+		 * Add attributes for site title element.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array  $attributes Existing attributes.
+		 * @param string $context    Attributes context.
+		 *
+		 * @return array Amended attributes.
+		 */
+		public function schema( $attributes = array(), $context = '' ) {
 
-				if ( isset( $this->attributes[ $context ] ) ) {
+			// Make sure we have the correct post type.
+			if ( get_post_type() !== $this->post_type ) {
+				return $attributes;
+			}
+
+			switch ( $context ) {
+				case 'entry':
+				case 'title':
+				case 'content':
+				case 'author':
 					return $this->add_schema( $context, $attributes );
-				}
+				default:
+					if ( isset( $this->attributes[ $context ] ) ) {
+						return $this->add_schema( $context, $attributes );
+					}
 
-				break;
+					break;
+			}
+
+			return $attributes;
 		}
 
-		return $attributes;
-	}
+		/**
+		 * Removes specific attributes from genesis attributes.
+		 *
+		 * @param array $attributes Array of Genesis attributes.
+		 *
+		 * @return array
+		 */
+		public static function remove( $attributes ) {
+			foreach (
+				array(
+					'itemtype',
+					'itemprop',
+					'itemscope',
+				) as $attr
+			) {
+				self::remove_attr( $attr, $attributes );
+			}
 
-	public static function remove( $attributes ) {
-		if ( isset( $attributes['itemtype'] ) ) {
-			$attributes['itemtype'] = '';
+			return $attributes;
 		}
-		if ( isset( $attributes['itemprop'] ) ) {
-			$attributes['itemprop'] = '';
+
+		/**
+		 * Removes specific key from array.
+		 *
+		 * @param string $attribute  Attribute to be removed.
+		 * @param array  $attributes Array of attributes.
+		 */
+		public static function remove_attr( $attribute, $attributes ) {
+			if ( isset( $attributes[ $attribute ] ) ) {
+				$attributes[ $attribute ] = '';
+			}
 		}
-		if ( isset( $attributes['itemscope'] ) ) {
-			$attributes['itemscope'] = '';
+
+		/**
+		 * Removes author name from attributes.
+		 *
+		 * @param array  $attributes Array of Genesis attributes.
+		 * @param string $context    Attributes context.
+		 *
+		 * @return mixed
+		 */
+		public function remove_author_name( $attributes, $context ) {
+			self::remove_attr( 'name', $attributes );
+
+			return $attributes;
 		}
 
-		return $attributes;
-	}
+		/**
+		 * Customizes Title Link.
+		 *
+		 * @param string $output Title HTML link tag.
+		 *
+		 * @return string
+		 */
+		public static function title_link( $output ) {
+			return str_replace( 'rel="author"', 'itemprop="author"', $output );
+		}
 
-	public function author_name( $attributes, $context ) {
-		$attributes['name'] = '';
-
-		return $attributes;
-	}
-
-	/**
-	 * Customizes Title Link
-	 *
-	 * @param $output
-	 *
-	 * @return mixed
-	 */
-	public static function title_link( $output ) {
-		return str_replace( 'rel="author"', 'itemprop="author"', $output );
-	}
-
-	/**
-	 * Remove the rel="author" and change it to itemprop="author" as the Structured Data Testing Tool doesn't understand
-	 * rel="author" in relation to Schema, even though it should according to the spec.
-	 *
-	 * @param $output
-	 *
-	 * @return mixed
-	 */
-	public static function author( $output ) {
-		return str_replace( 'rel="bookmark"', 'rel="bookmark" itemprop="itemprop"', $output );
+		/**
+		 * Remove the rel="author" and change it to itemprop="author" as the Structured Data Testing Tool doesn't understand
+		 * rel="author" in relation to Schema, even though it should according to the spec.
+		 *
+		 * @param string $output Output HTML.
+		 *
+		 * @return mixed
+		 */
+		public static function author( $output ) {
+			return str_replace( 'rel="bookmark"', 'rel="bookmark" itemprop="itemprop"', $output );
+		}
 	}
 }
 
